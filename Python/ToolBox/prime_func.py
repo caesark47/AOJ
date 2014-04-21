0d3ab163d1ab9f97e392e280081c93a014536282
caesark <caesark47@gmail.com>
2014-04-22 02:48:13 +0900

    まったくもって速くなかったので高速化

--- Python/ToolBox/prime_func.py
+++ Python/ToolBox/prime_func.py
@@ -1,19 +1,11 @@
-import math
-def get_primes(goal):
-	n_list = range(3,goal+1,2)
-	primes = [2]
-	sieve = map(lambda n:[n,1],n_list)#Sieve of Eratosthenes
-	while sieve[0][0] < math.sqrt(goal+1):
-		target = sieve[0][0]
-		primes = primes + [target]
-		i = 0
-		while i < len(sieve):
-			sieve[i][1] = 0
-			i += target
-		while  sieve[0][1] == 0:
-			sieve = sieve[1:]
-	primes = primes + map(lambda x:x[0],filter(lambda n:n[1] ,sieve))
-	return primes
+def get_primes(n):
+    primes = [True] * n
+    primes[0] = primes[1] = False
+    for i in xrange(2, int(n ** 0.5) + 1):
+        if primes[i]:
+            for j in xrange(i ** 2, n, i):
+                primes[j] = False
+    return [i for i in xrange(2, n) if primes[i]]
 
 
 primes = get_primes(100000)
